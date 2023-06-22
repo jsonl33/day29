cc637ade1881a4114328c460849703b30f5d8706
testuser <testuser@test.com>
2023-06-22 21:48:43 +0900

    oracle

--- src/guestbook.sql
+++ src/guestbook.sql
@@ -25,4 +25,11 @@
 
 --update guest set gtitle='수정제목', gcont='수정내용' where gno=2;
 
-delete from guest where gno=3;
+--delete from guest where gno=3;
+
+insert into guest(gno, gname, gtitle, gcont, gdate)
+values (21, '을지문덕', '제목21', '내용21', sysdate);
+
+select * from guest where gno=21;
+
+update guest set gname='강감찬', gtitle='수정제목', gcont='수정내용21' where gno=21;
