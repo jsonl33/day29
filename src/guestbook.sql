14783edceb9c3d3d3bfbf2a60bee4e7ae41cf7b4
testuser <testuser@test.com>
2023-06-22 21:28:41 +0900

    oracle

---
+++ src/guestbook.sql
@@ -0,0 +1,28 @@
+--create table guest(
+--gno number(38) primary key,
+--gname varchar2(20) not null,
+--gtitle varchar2(200) not null,
+--gcont varchar2(4000) not null,
+--gdate date
+--);
+
+--create SEQUENCE gno_seq
+--start with 1
+--increment by 1
+--nocache;
+
+--select gno_seq.nextval as "시퀀스번호값" from dual;
+
+--insert into guest(gno, gname, gtitle, gcont, gdate) 
+--VALUES (gno_seq.nextval, '홍길동', '방명록제목01', '방명록내용01', sysdate);
+
+--select * from guest;
+
+--insert into guest(gno, gname, gtitle, gcont, gdate) 
+--VALUES (gno_seq.nextval, '이순신', '방명록제목02', '방명록내용02', sysdate);
+
+--select * from guest order by gno desc;
+
+--update guest set gtitle='수정제목', gcont='수정내용' where gno=2;
+
+delete from guest where gno=3;
